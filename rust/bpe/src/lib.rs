0243b393a3db660b0a39505c5cbf7f0b9e9c1efb
Abdullah Aml <a.abdelfattah@wakeb.tech>
2026-08-18 18:07:25 +0300

    add: inital version of chunk_boundries in rust

--- rust/bpe/src/lib.rs
+++ rust/bpe/src/lib.rs
@@ -1,7 +1,6 @@
 use boltffi::*;
-use std::collections::HashMap;
 use std::fs::File;
-use std::path::Path;
+use std::io::{Read, Seek, SeekFrom};
 
 #[export]
 pub fn add(left: u64, right: u64) -> u64 {
@@ -9,7 +8,7 @@ pub fn add(left: u64, right: u64) -> u64 {
 }
 
 pub fn find_chunk_boundries(
-    file: &File,
+    file: &mut File,
     desired_num_chunks: usize,
     split_special_token: &[u8],
 ) -> Vec<u64> {
@@ -23,9 +22,35 @@ pub fn find_chunk_boundries(
     chunk_boundries.push(file_size);
 
     println!("chunk_boundary");
-    let mini_chunk_size = 4096; // read ahead a mini chunk (small chunk) this no minimum
+    let mut buffer: [u8; 4096] = [0; 4096]; // a buffer in stack fast
+    for bi in 1..chunk_boundries.len() - 1 {
+        let mut initial_position = chunk_boundries[bi];
+        file.seek(SeekFrom::Start(initial_position))
+            .expect("Could not seek file with boundary");
+        loop {
+            let num_bytes_read = file.read(&mut buffer).expect("Error reading the buffer");
 
-    vec![1, 2, 3]
+            // EOF
+            if num_bytes_read == 0 {
+                break;
+            }
+
+            // searching the position of special_token
+            let idx_opt = buffer[..num_bytes_read]
+                .windows(split_special_token.len())
+                .position(|w| w == split_special_token);
+            if let Some(idx) = idx_opt {
+                // update the index of the indices vector
+                chunk_boundries[bi] = idx as u64 + initial_position;
+                break;
+            }
+
+            // it is not granuteed to return exaclly 4096 bytes
+            initial_position += num_bytes_read as u64;
+        }
+    }
+
+    chunk_boundries
 }
 
 // struct TrainedBPEOut {
--- rust/bpe/src/main.rs
+++ rust/bpe/src/main.rs
@@ -2,10 +2,24 @@ use bpe::find_chunk_boundries;
 use std::fs::File;
 
 fn main() {
-    let file = File::open("../../AGENTS.md").unwrap();
+    let mut file = File::open("../../AGENTS.md").unwrap();
     dbg!(&file);
 
     let special_tokens = [1, 2, 3];
-    let out_vec = find_chunk_boundries(&file, 4, &special_tokens);
+    let out_vec = find_chunk_boundries(&mut file, 4, &special_tokens);
     dbg!(&out_vec);
+
+    let arr: [i32; 10] = std::array::from_fn(|i| i as i32);
+    let v: Vec<&[i32]> = arr.windows(3).collect();
+    dbg!(&v);
+    let found_at = arr.windows(3).position(|w| w == [3, 4, 5]);
+    dbg!(&found_at);
+
+    let mut v = vec![1, 2, 3, 4];
+    let mut val = v[3];
+    dbg!(val);
+    v[3] = 10;
+    val = 0;
+    dbg!(v);
+    dbg!(val);
 }
